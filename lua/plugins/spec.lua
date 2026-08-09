ebe9103f3848eecb57eecc3cf8528c11a5f944e0
George Kousis <gmkousis@gmail.com>
2026-08-10 00:21:40 +0300

    personalize lualine

--- lua/plugins/spec.lua
+++ lua/plugins/spec.lua
@@ -164,7 +164,29 @@ return {
         'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
         config = function()
-            require("lualine").setup()
+            require("lualine").setup({
+                winbar = {
+                    lualine_c = { { 'filename', path = 0, file_status = true} },
+                },
+                inactive_winbar = {
+                    lualine_c = { { 'filename', path = 0, file_status = true } },
+                },
+                tabline = {
+                },
+                -- bottom
+                sections = {
+                    lualine_a = {'mode'},
+                    lualine_b = {'diagnostics'},
+                    lualine_c = {'branch'},
+                    lualine_x = {'filetype'},
+                    lualine_y = {'progress'},
+                    lualine_z = {'location'}
+                },
+                inactive_sections = {
+                    lualine_c = {},
+                    lualine_x = {}
+                }
+            })
         end,
     }
 }
