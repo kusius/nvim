d2cbcb3cec61ed4cbd66b36c2abf896a99d75379
George Kousis <gmkousis@gmail.com>
2026-08-10 21:33:52 +0300

    Feat: Jumps to last cursor position when reopening a closed buffer

--- init.lua
+++ init.lua
@@ -11,6 +11,19 @@ vim.opt.softtabstop = 4    -- Number of spaces for <Tab> in insert mode
 vim.opt.ignorecase = true  -- Ignore case in searches
 vim.opt.smartcase = true   -- Override ignorecase if search contains uppercase
 
+-- Automatically go to the last cursor position when reopening a closed buf
+-- FYI: double quotes (") is the special marker denoting the last cursor position
+-- before the buffer was closed.
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group = vim.api.nvim_create_augroup('RestoreCursorPosition', { clear = true }),
+  callback = function(args)
+    local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+    if mark[1] > 0 and mark[1] <= vim.api.nvim_buf_line_count(args.buf) then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
 vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'c3', 'ruby', 'eruby', 'scss', 'css', 'html',
               'javascript', 'javascriptreact', 'yaml'},
